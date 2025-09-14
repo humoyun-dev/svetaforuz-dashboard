0cbc4fdc95c0739311b68be11ccd5bd3306365bd
Humoyunbek Tursunniyazov <humoyunbektursunniyazov@gmail.com>
2025-09-14 21:53:43 +0500

    dockerize

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
   env: {
     // NEXT_PUBLIC_SERVER_URL: "http://localhost:8000/platform/",
     NEXT_PUBLIC_WS_URL: "ws://localhost:8000/ws/notifications/",
-    NEXT_PUBLIC_SERVER_URL: "https://server.svetafor.uz/platform/",
+    NEXT_PUBLIC_SERVER_URL: "https://api.svetafor.uz/platform/",
     NEXT_PUBLICf_YANDEX_MAPS_API_KEY: "c9a08bdc-e348-4de7-b9b4-b4f02f5fd7f7",
   },
 };
